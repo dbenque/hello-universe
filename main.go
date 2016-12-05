629c3c44b6ad4e7c91102e9e48b6e05d8684a02a
David Benque <dbenque@gmail.com>
2016-12-05 07:46:52 +0100

    test with minikube

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"syscall"
 
 	"github.com/kelseyhightower/kargo"
@@ -34,11 +38,38 @@ func main() {
 
 	var dm *kargo.DeploymentManager
 	if kargo.EnableKubernetes {
-		link, err := kargo.Upload(kargo.UploadConfig{
+		// link, err := kargo.Upload(kargo.UploadConfig{
+		// 	ProjectID:  "hightowerlabs",
+		// 	BucketName: "hello-universe",
+		// 	ObjectName: "hello-universe",
+		// })
+		link := ""
+		config := kargo.UploadConfig{
 			ProjectID:  "hightowerlabs",
 			BucketName: "hello-universe",
 			ObjectName: "hello-universe",
-		})
+		}
+
+		tmpDir, err := ioutil.TempDir("", "")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if config.Path == "" {
+			fmt.Printf("Building %s binary...\n", config.ObjectName)
+			output, err := build(tmpDir, config.ObjectName)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			config.Path = output
+			fmt.Println("Created: " + config.Path)
+			fmt.Println("Serving file")
+			_, f := path.Split(output)
+			link = "http://178.0.0.1:8090/" + f
+			go http.ListenAndServe(":8090", http.FileServer(http.Dir(tmpDir)))
+		}
 
 		if err != nil {
 			fmt.Println(err)
@@ -48,6 +79,7 @@ func main() {
 		env := make(map[string]string)
 		env["HELLO_UNIVERSE_TOKEN"] = os.Getenv("HELLO_UNIVERSE_TOKEN")
 
+		fmt.Println("Going on with Kargo")
 		dm = kargo.New()
 		err = dm.Create(kargo.DeploymentConfig{
 			Args:      []string{"-http=0.0.0.0:80"},
@@ -93,4 +125,35 @@ func main() {
 			os.Exit(0)
 		}
 	}
+}
+
+func build(tmpDir, name string) (string, error) {
+	output := filepath.Join(tmpDir, name)
+
+	ldflags := `-extldflags "-static"`
+	command := []string{
+		"go", "build", "-o", output, "-a", "--ldflags",
+		ldflags, "-tags", "netgo",
+		"-installsuffix", "netgo", ".",
+	}
+	cmd := exec.Command(command[0], command[1:]...)
+
+	gopath := os.Getenv("GOPATH")
+	goroot := os.Getenv("GOROOT")
+	path := os.Getenv("PATH")
+	cmd.Env = []string{
+		"GOOS=linux",
+		"GOARCH=amd64",
+		"GOPATH=" + gopath,
+		"GOROOT=" + goroot,
+		"PATH=" + path,
+	}
+
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(data))
+		return "", err
+	}
+
+	return output, nil
 }
